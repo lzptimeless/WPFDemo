5f958ccee8439f45d4e200c5f96be4bc2e14df6a
zhongping.ling <zhongping.ling@razer.com>
2023-08-17 16:58:18 +0800

    完成所有样式

--- src/WinUI3Demo/MainWindow.xaml.cs
+++ src/WinUI3Demo/MainWindow.xaml.cs
@@ -10,8 +10,11 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
@@ -56,8 +59,103 @@ namespace WinUI3Demo
             SetTitleBar(CustomTitleBar);
         }
 
+        #region properties
+        public TimespanPickerData TimespanPickerData { get; } = new TimespanPickerData();
+        #endregion
+
         private void Root_Loaded(object sender, RoutedEventArgs e)
         {
+            
+        }
+
+        private void Flyout_Opened(object sender, object e)
+        {
+            TimespanListBoxHours.ScrollIntoView(TimespanPickerData.Hours);
+            TimespanListBoxMinutes.ScrollIntoView(TimespanPickerData.Minutes);
+            TimespanListBoxSeconds.ScrollIntoView(TimespanPickerData.Seconds);
+        }
+    }
+
+    public class TimespanPickerData : INotifyPropertyChanged
+    {
+        public TimespanPickerData()
+        {
+            for (int i = 0; i < 24; i++)
+            {
+                AllHours.Add(i);
+            }
+
+            for (int i = 0; i < 60; i++)
+            {
+                AllMinutes.Add(i);
+            }
+
+            for (int i = 0; i < 60; i++)
+            {
+                AllSeconds.Add(i);
+            }
+
+            _hours = 12;
+            _minutes = 16;
+            _seconds = 3;
+        }
+
+        public List<int> AllHours { get; } = new List<int>();
+        public List<int> AllMinutes { get; } = new List<int>();
+        public List<int> AllSeconds { get; } = new List<int>();
+        private int _hours;
+        public int Hours
+        {
+            get { return _hours; }
+            set
+            {
+                if (value == _hours) return;
+
+                _hours = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Text));
+            }
+        }
+        private int _minutes;
+        public int Minutes
+        {
+            get { return _minutes; }
+            set
+            {
+                if (value == _minutes) return;
+
+                _minutes = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Text));
+            }
+        }
+        private int _seconds;
+        public int Seconds
+        {
+            get { return _seconds; }
+            set
+            {
+                if (value == _seconds) return;
+
+                _seconds = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Text));
+            }
+        }
+        public string Text
+        {
+            get
+            {
+                return $"{Hours:00}:{Minutes:00}:{Seconds:00}";
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            // Raise the PropertyChanged event, passing the name of the property whose value has changed.
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
